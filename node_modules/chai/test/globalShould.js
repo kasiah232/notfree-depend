4b8c8654c63cbb1e6f9b7ef4586cb6b6c1c17f7c
kasiah232 <kasiaherman232@gmail.com>
2025-01-16 12:38:10 +0100

    package

---
+++ node_modules/chai/test/globalShould.js
@@ -0,0 +1,17 @@
+import * as chai from '../index.js';
+
+describe('global should', function () {
+  it('works', function () {
+    var theGlobal = typeof window !== 'undefined'
+      ? window
+      : global;
+
+    theGlobal.globalShould = chai.should();
+
+    try {
+        globalShould.not.exist(undefined);
+    } finally {
+        delete theGlobal.globalShould;
+    }
+  });
+});
